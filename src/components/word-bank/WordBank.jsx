a6e70f467cdf10e78812f56eab692edaface1142
Kevin Huang <khuan041@gmail.com>
2021-12-12 22:36:22 -0800

    create wordbank

---
+++ src/components/word-bank/WordBank.jsx
@@ -0,0 +1,19 @@
+
+const WordBank = [
+    { id: 0, pinyin: "huà", hanzi: "話" },
+    { id: 1, pinyin: "shuō", hanzi: "說" },
+    { id: 2, pinyin: "tiān", hanzi: "天" },
+    { id: 3, pinyin: "xià", hanzi: "下" },
+    { id: 4, pinyin: "dà", hanzi: "大" },
+    { id: 5, pinyin: "shì", hanzi: "勢" },
+    { id: 6, pinyin: "fēn", hanzi: "分" },
+    { id: 7, pinyin: "jiǔ", hanzi: "久" },
+    { id: 8, pinyin: "bì", hanzi: "必" },
+    { id: 9, pinyin: "hé", hanzi: "合" },
+    { id: 10, pinyin: "shǔ", hanzi: "蜀" },
+    { id: 11, pinyin: "hàn", hanzi: "漢" },
+    { id: 12, pinyin: "wèi", hanzi: "魏" },
+    { id: 13, pinyin: "wú", hanzi: "呉" },
+]
+
+export default WordBank;
