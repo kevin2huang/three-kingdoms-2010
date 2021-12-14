61dcefb494f087edf0daa799943f163bd8d4cdd5
Kevin Huang <khuan041@gmail.com>
2021-12-13 20:36:12 -0800

    store character data

--- src/components/characters/CharacterData.jsx
+++ src/components/characters/CharacterData.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-const ShuHan = [
+
+export const ShuHan = [
   { img: "img_07", mingzi: "劉備 (玄德)", name: "Liu Bei (Xuande)" },
   { img: "img_08", mingzi: "關羽 (雲長)", name: "Guan Yu (Yunchang)" },
   { img: "img_09", mingzi: "張飛 (益德)", name: "Zhang Fei (Yide)" },
@@ -10,133 +8,44 @@ const ShuHan = [
   { img: "img_10", mingzi: "諸葛亮 (孔明)", name: "Zhuge Liang (Kongming)" },
   { img: "img_10", mingzi: "馬超 (孟起)", name: "Ma Chao (Mengqi)" },
   { img: "img_10", mingzi: "劉協 (伯和)", name: "Liu Xie (Bohe)" },
+  { img: "img_12", mingzi: "曹節", name: "Cao Jie" },
+  { img: "img_10", mingzi: "劉表 (景升)", name: "Liu Biao (Jingsheng)" },
+  { img: "img_10", mingzi: "陶謙 (恭祖)", name: "Tao Qian (Gongzu)" },
+  { img: "img_10", mingzi: "曹豹", name: "Cao Bao" },
 ];
 
-const Wei = [
+export const Wei = [
   { img: "img_12", mingzi: "曹操 (孟德)", name: "Cao Cao (Mengde)" },
   { img: "img_12", mingzi: "曹丕 (子桓)", name: "Cao Pi (Zihuan)" },
+  { img: "img_12", mingzi: "曹彰 (子文)", name: "Cao Zhang (Ziwen)" },
+  { img: "img_12", mingzi: "曹植 (子建)", name: "Cao Zhi (Zijian)" },
+  { img: "img_12", mingzi: "曹沖 (倉舒)", name: "Cao Chong (Cangshu)" },
   { img: "img_12", mingzi: "司馬懿 (仲達)", name: "Sima Yi (Zhongda)" },
+  { img: "img_12", mingzi: "曹叡 (元仲)", name: "Cao Rui (Yuanzhong)" },
+  { img: "img_12", mingzi: "曹芳 (蘭卿)", name: "Cao Fang (Lanqing)" },
+  { img: "img_12", mingzi: "許褚 (仲康)", name: "Xu Chu (Zhongkang)" },
+  { img: "img_12", mingzi: "曹洪 (子廉)", name: "Cao Hong (Zilian)" },
+  { img: "img_12", mingzi: "曹仁 (子孝)", name: "Cao Ren (Zixiao)" },
+  { img: "img_12", mingzi: "曹真 (子丹)", name: "Cao Zhen (Zidan)" },
+  { img: "img_12", mingzi: "曹爽 (昭伯)", name: "Cao Shuang (Zhaobo)" },
+  { img: "img_12", mingzi: "蔡瑁 (德珪)", name: "Cai Mao (Degui)" },
+  { img: "img_12", mingzi: "陳登 (元龍)", name: "Chen Deng (Degui)" },
 ];
 
-const Wu = [
+export const Wu = [
   { img: "img_07", mingzi: "孫堅 (文臺)", name: "Sun Jian (Wentai)" },
+  { img: "img_07", mingzi: "孫策 (伯符)", name: "Sun Ce (Bofu)" },
+  { img: "img_07", mingzi: "孫權 (仲謀)", name: "Sun Quan (Zhongmou)" },
+  { img: "img_07", mingzi: "周瑜 (公瑾)", name: "Zhou Yu (Gongjin)" },
+  { img: "img_07", mingzi: "孫夫人", name: "Lady Sun" },
+  { img: "img_07", mingzi: "大橋", name: "Da Qiao" },
+  { img: "img_07", mingzi: "小橋", name: "Xiao Qiao" },
+  { img: "img_07", mingzi: "程普 (德謀)", name: "Cheng Pu (Demou)" },
 ];
 
-const Other = [
+export const Other = [
   { img: "img_12", mingzi: "董卓 (仲穎)", name: "Dong Zhuo (Zhongying)" },
+  { img: "img_12", mingzi: "呂布 (奉先)", name: "Lu Bu (Fengxian)" },
+  { img: "img_12", mingzi: "貂蟬", name: "Diao Chan" },
+  { img: "img_12", mingzi: "陳宮 (公臺)", name: "Chen Gong (Gongtai)" },
 ];
-
-const CharacterData = () => {
-  return (
-    <>
-      <Tabs>
-        <TabList className="controls d-flex align-items-center justify-content-center mb-100 md-mb-60">
-          <Tab>
-            <button type="button" className="control">
-              Shu-Han
-            </button>
-          </Tab>
-          <Tab>
-            <button type="button" className="control">
-              Wei
-            </button>
-          </Tab>
-          <Tab>
-            <button type="button" className="control">
-              Wu
-            </button>
-          </Tab>
-          <Tab>
-            <button type="button" className="control">
-              Other
-            </button>
-          </Tab>
-          {/* <Tab>
-            <button type="button" className="control">
-              Marketing
-            </button>
-          </Tab> */}
-        </TabList>
-        {/* End .Tablist */}
-        <TabPanel>
-          <div className="mixitUp-container">
-            {ShuHan.map((item, i) => (
-              <Link to="/team-details-v1" className="mix" key={i}>
-                <div className="team-member">
-                  <img src={`images/media/${item.img}.png`} alt="media" />
-                  <h4>{item.mingzi}</h4>
-                  <strong>{item.name}</strong>
-                </div>
-                {/* /.team-member */}
-              </Link>
-            ))}
-
-            <div className="gap"></div>
-            <div className="gap"></div>
-            <div className="gap"></div>
-          </div>
-          {/* /.mixitUp-container */}
-        </TabPanel>
-        <TabPanel>
-          <div className="mixitUp-container">
-            {Wei.map((item, i) => (
-              <Link to="/team-details-v1" className="mix" key={i}>
-                <div className="team-member">
-                  <img src={`images/media/${item.img}.png`} alt="media" />
-                  <h4>{item.mingzi}</h4>
-                  <strong>{item.name}</strong>
-                </div>
-                {/* /.team-member */}
-              </Link>
-            ))}
-
-            <div className="gap"></div>
-            <div className="gap"></div>
-            <div className="gap"></div>
-          </div>
-          {/* /.mixitUp-container */}
-        </TabPanel>
-        <TabPanel>
-          <div className="mixitUp-container">
-            {Wu.map((item, i) => (
-              <Link to="/team-details-v1" className="mix" key={i}>
-                <div className="team-member">
-                  <img src={`images/media/${item.img}.png`} alt="media" />
-                  <h4>{item.mingzi}</h4>
-                  <strong>{item.name}</strong>
-                </div>
-                {/* /.team-member */}
-              </Link>
-            ))}
-
-            <div className="gap"></div>
-            <div className="gap"></div>
-            <div className="gap"></div>
-          </div>
-          {/* /.mixitUp-container */}
-        </TabPanel>
-        <TabPanel>
-          <div className="mixitUp-container">
-            {Other.map((item, i) => (
-              <Link to="/team-details-v1" className="mix" key={i}>
-                <div className="team-member">
-                  <img src={`images/media/${item.img}.png`} alt="media" />
-                  <h4>{item.mingzi}</h4>
-                  <strong>{item.name}</strong>
-                </div>
-                {/* /.team-member */}
-              </Link>
-            ))}
-
-            <div className="gap"></div>
-            <div className="gap"></div>
-            <div className="gap"></div>
-          </div>
-          {/* /.mixitUp-container */}
-        </TabPanel>
-      </Tabs>
-    </>
-  );
-};
-
-export default CharacterData;
