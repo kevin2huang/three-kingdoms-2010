f112f4c24b7fe84467dbef118e98127f24175b50
Kevin Huang <khuan041@gmail.com>
2021-12-14 21:37:10 -0800

    create sidebar menu file

---
+++ src/components/episodes/SideBarMenu.jsx
@@ -0,0 +1,81 @@
+
+export const SideBarMenu = [
+    {
+      title: "Introduction",
+      subMenuContent: [
+        {
+          itemName: "Opening Theme Song",
+          activeClass: "active",
+          link: "",
+        },
+        {
+          itemName: " Installation",
+          activeClass: "",
+          link: "",
+        },
+      ],
+      expand: "intro",
+    },
+    {
+      title: " Episode 1",
+      subMenuContent: [
+        {
+          itemName: "Theme color",
+          activeClass: "",
+          link: "",
+        },
+      ],
+      expand: "ep1",
+    },
+  
+    {
+      title: "Episode 2",
+      subMenuContent: [
+        {
+          itemName: "Box Layout",
+          activeClass: "",
+          link: "",
+        },
+        {
+          itemName: "Full width Layout",
+          activeClass: "",
+          link: "",
+        },
+      ],
+      expand: "ep2",
+    },
+  
+    {
+      title: "Episode 3",
+      subMenuContent: [
+        {
+          itemName: "HTML Structure",
+          activeClass: "",
+          link: "",
+        },
+        {
+          itemName: "Favicon Settings",
+          activeClass: "",
+          link: "",
+        },
+      ],
+      expand: "ep3",
+    },
+  
+    {
+      title: "Episode 4",
+      subMenuContent: [
+        {
+          itemName: "Getting started",
+          activeClass: "",
+          link: "",
+        },
+        {
+          itemName: " Subscriptions",
+          activeClass: "",
+          link: "",
+        },
+      ],
+      expand: "ep4",
+    },
+  ];
